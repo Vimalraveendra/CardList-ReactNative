c403a53965e6053ef1c6a8768bad46c5df9c0b51
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-07 12:53:42 +0200

    updated App component to display card

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Card List</Text>
+      <TouchableOpacity style={styles.card}>
+        <Image
+          source={{
+            uri:
+              'https://cdn.pixabay.com/photo/2020/09/01/06/10/lake-5534341_1280.jpg',
+          }}
+          style={styles.cardImage}
+        />
+        <Text style={styles.cardTitle}>Card List</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 50,
   },
-  title: {
-    fontSize: 23,
-    fontWeight: '700',
+
+  card: {
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    marginLeft: '5%',
+    width: '90%',
+    shadowColor: '#000',
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    borderRadius: 10,
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+  cardTitle: {
+    fontSize: 16,
+    padding: 10,
   },
 });
 
